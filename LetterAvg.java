60973c72789997e8b0c84155f4254198472e520d
William Kett <william.kett-1@ou.edu>
2019-10-09 15:27:12 -0500

    Determined how to initialize arraylist created in class field. Very dumb mistake to make to be honest.

--- LetterAvg.java
+++ LetterAvg.java
@@ -6,8 +6,15 @@ MesoInherit MesoObject;
 protected ArrayList<String> MesoList;
 protected ArrayList<String> LetterAvgList;
 public LetterAvg(char letterAverage) {
-	//System.out.println(letterAverage);
-	//
+	
+	this.LetterAvgList = new ArrayList<String>();
+	
+	try {
+		MesoObject = new MesoInherit();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	try {
 		MesoObject = new MesoInherit();
 	} catch (IOException e) {
@@ -16,17 +23,20 @@ public LetterAvg(char letterAverage) {
 	}
 	MesoList = MesoObject.getMesoList();
 	for (int i = 0; i < MesoList.size(); i++) {
-		if(MesoList.get(i).charAt(0) == letterAverage) {
+		if(MesoList.get(i).charAt(1) == letterAverage) {
 			LetterAvgList.add(MesoList.get(i));
+			System.out.println(MesoList.get(i).charAt(1));
 		}
 	}
+		
+	
 }
 	public String numberOfStationWithLetterAvg() {
 		// TODO Auto-generated method stub
 		String str = "";
-		for (int i = 0; i < LetterAvgList.size();i++) {
-			str += LetterAvgList.get(i) + "\n";
-		}
+		//for (int i = 0; i < LetterAvgList.size();i++) {
+		//	str += LetterAvgList.get(i) + "\n";
+		//}
 		return str;
 	}
 
